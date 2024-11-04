a31661cdee255b948714990409922728684bbdfd
Pamela <pamela.isabeli167@gmail.com>
2024-11-04 09:02:28 -0300

    OK

--- InfinityRunner/MainPage.xaml.cs
+++ InfinityRunner/MainPage.xaml.cs
@@ -2,8 +2,8 @@
 
 public partial class MainPage : ContentPage
 {
-	bool estMorto=false;
-	bool estPulando=false;
+	bool estaMorto=false;
+	bool estaPulando=false;
 	const int tempoEntreFrames=25;
 	int velocidade1=0;
 	int velocidade2=0;
@@ -21,6 +21,11 @@ public partial class MainPage : ContentPage
 		CorrigeTamanhoCenario(w,h);
 		CalculaVelocidade(w);
     }
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenha();
+	}
 	void CalculaVelocidade(double w)
 	{
 		velocidade1=(int)(w*0.001);
@@ -29,20 +34,60 @@ public partial class MainPage : ContentPage
 	}
 	void CorrigeTamanhoCenario(double w, double h)
 	{
-		foreach(var a in HSLayer1.Children)
+		foreach(var a in layerFundo.Children)
 		(a as Image ).WidthRequest = w;
 		foreach(var a in HSLayer2.Children)
 		(a as Image ).WidthRequest = w;
 		foreach (var a in HSLayer3.Children)
 		(a as Image ).WidthRequest = w;
-		foreach( var a in HSLayerChao.Children)
+		foreach( var a in layerAsfalto.Children)
 		(a as Image ).WidthRequest = w;
 
-		HSLayer1.WidthRequest=w*1.5;
+		layerFundo.WidthRequest=w*1.5;
 		HSLayer2.WidthRequest=w*1.5;
 		HSLayer3.WidthRequest=w*1.5;
-		HSLayerChao.WidthRequest=w*1.5;
+		HSLayerAsfalto.WidthRequest=w*1.5;
 	}
+	void GerenciaCenarios()
+	{
+		MoveCenario();
+		GerenciaCenario(layerFundo);
+		GerenciaCenario(HSLayer2);
+		GerenciaCenario(HSLayer3);
+		GerenciaCenario(layerAsfalto);
+	}
+	void MoveCenario()
+	{
+	
+		layerFundo.TranslationX -= velocidade1;
+		HSLayer2.TranslationX -= velocidade2;
+		HSLayer3.TranslationX -= velocidade3;
+		layerAsfalto.TranslationX -= velocidade;
+	}
+	
+	void GerenciaCenario(HorizontalStackLayout hsl)
+	{
+		var view = (hsl.Children.First() as Image);
+		if(view.WidthRequest+hsl.TranslationX<0)
+		{
+			hsl.Children.Remove(view);
+			hsl.Children.Add(view);
+			hsl.TranslationX = view.TranslationX;
+		}
+	}
+	async Task Desenha()
+	{
+		while(!estaMorto)
+		{
+			GerenciaCenarios();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+	
+	
+	
+
+
 
 	
 }
